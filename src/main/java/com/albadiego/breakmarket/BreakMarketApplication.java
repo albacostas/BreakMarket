e348134e1161d81dec28eb409eb23f03b3365b43
Diego <diegopalvarez@gmail.com>
2026-09-16 11:15:32 +0200

    Configurar proyecto

---
+++ src/main/java/com/albadiego/breakmarket/BreakMarketApplication.java
@@ -0,0 +1,13 @@
+package com.albadiego.breakmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreakMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/albadiego/breakmarket/BreakMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albadiego.breakmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreakMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
